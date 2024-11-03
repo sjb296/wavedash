e8065ff8c511503a6b33daaa7af17fea2704ff68
Sam Barker <sambarker247@gmail.com>
2024-11-03 10:36:49 +0000

    Added Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="flex justify-center">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,7 +18,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)}
+          className="bg-blue-200">
           <code>count</code> is {count}
         </button>
         <button onClick={() => setCount(() => 1)}>
