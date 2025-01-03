a1449f09615ab42b051fd49cca0037347bef3b19
Sam Barker <sambarker247@gmail.com>
2025-01-03 16:39:52 +0000

    Added dark mode for start screen and skeletons

--- src/components/SkeletonCarousel/SkeletonCarousel.tsx
+++ src/components/SkeletonCarousel/SkeletonCarousel.tsx
@@ -7,11 +7,10 @@ const SkeletonCarousel = ({ rows, cols, className }: { rows: number, cols: numbe
       {Array.from({ length: rows }).map((_, index) => (
         <div
           key={index}
-          className="h-4 w-1/1 bg-gray-300 rounded-md animate-pulse mx-2"
+          className="h-4 w-1/1 bg-gray-300 dark:bg-slate-800 rounded-md animate-pulse mx-2"
         />
       ))}
     </div>
-
   ))
 
   return (
--- src/components/StartScreen/StartScreen.tsx
+++ src/components/StartScreen/StartScreen.tsx
@@ -14,7 +14,7 @@ const StartScreen = (
   return (
     <div
       id="start-screen"
-      className={"fixed inset-0 bg-white w-screen h-screen z-40 flex justify-center items-center align-middle p-12 " + className}>
+      className={"fixed inset-0 bg-white dark:bg-black w-screen h-screen z-40 flex justify-center items-center align-middle p-12 " + className}>
       <div className="text-center flex flex-col gap-4 items-center">
         {/* Logo */}
         <img src="/images/wavedash-high-resolution-logo-transparent.svg" className="h-20" alt="Wavedash logo" />
@@ -24,11 +24,11 @@ const StartScreen = (
             ?
             <>
               <h1 className="text-2xl font-medium">Welcome to Wavedash!</h1>
-              <p className="text-lg text-slate-500">
+              <p className="text-lg">
                 This app requires your location to provide accurate weather data.
                 Please allow access to your location to continue.
               </p>
-              <button className="bg-blue-400 text-white rounded-3xl py-3 px-5 m-0 font-medium w-36"
+              <button className="bg-blue-400 dark:bg-blue-600 rounded-3xl py-3 px-5 m-0 font-medium w-36"
                 onClick={() => {
                   getLocation()
                   setIsLoading(true)
