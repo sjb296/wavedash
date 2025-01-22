21b58133769fb132f59a8d5c4b161a3e398c1e7e
Sam Barker <sambarker247@gmail.com>
2025-01-22 17:20:42 +0000

    Removed service worker

--- public/sw.js
+++
@@ -1,9 +0,0 @@
-import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching';
-
-cleanupOutdatedCaches();
-
-precacheAndRoute(self.__WB_MANIFEST);
-
-self.addEventListener("activate", () => {
-    console.log("SW activating...");
-});
